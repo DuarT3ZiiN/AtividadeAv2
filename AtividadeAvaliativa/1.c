0f0ac137f0a766f331886436c73c70da3c07c251
Cleber Duarte <135433935+DuarT3ZiiN@users.noreply.github.com>
2023-11-20 22:53:46 -0300

    Add files via upload

---
+++ AtividadeAvaliativa/1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int validaQuantidade(int quantidade)
+{
+    if (quantidade >= 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+float calculaSalario(int quantidade)
+{
+    float salarioBase = 600.0;
+    float adicional = 0.0;
+
+    if (quantidade > 50)
+    {
+        adicional += (quantidade - 50) * 0.5;
+    }
+    if (quantidade > 80)
+    {
+        adicional += (quantidade - 80) * 0.25;
+    }
+
+    return salarioBase + adicional;
+}
+
+void mostraFinal(int quantidade, float salario)
+{
+    printf("Quantidade de pecas fabricadas: %d\n", quantidade);
+    printf("Salario total: R$ %.2f\n", salario);
+    printf("\n");
+}
+
+int main()
+{
+    int quantidade;
+
+    while (1)
+    {
+        printf("Digite a quantidade de pecas fabricadas (ou um valor negativo para encerrar): ");
+        scanf("%d", &quantidade);
+
+        if (quantidade < 0)
+        {
+            break;
+        }
+
+        if (validaQuantidade(quantidade))
+        {
+            float salario = calculaSalario(quantidade);
+            mostraFinal(quantidade, salario);
+        }
+        else
+        {
+            printf("Quantidade invalida. Tente novamente.\n");
+        }
+    }
+
+    return 0;
+}
